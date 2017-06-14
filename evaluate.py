7244d08ad0da4dddf98fcf59c9bfd51e44149378
supercoderhawk <supercoderhawk@gmail.com>
2017-06-14 16:58:54 +0800

    modify test code

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,16 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 import numpy as np
 from dnn import DNN
 from prepare_data import PrepareData
 from config import CorpusType
 
-def estimate_cws(current_labels,correct_labels):
+
+def estimate_cws(current_labels, correct_labels):
   cor_dict = {}
   curt_dict = {}
   curt_start = 0
   cor_start = 0
-  for label_index,(curt_label,cor_label) in enumerate(zip(current_labels,correct_labels)):
+  for label_index, (curt_label, cor_label) in enumerate(zip(current_labels, correct_labels)):
     if cor_label == 0:
       cor_dict[label_index] = label_index + 1
     elif cor_label == 1:
@@ -31,22 +32,24 @@ def estimate_cws(current_labels,correct_labels):
     if curt_start in cor_dict and curt_dict[curt_start] == cor_dict[curt_start]:
       cor_count += 1
 
-  return  cor_count,prec_length,recall_length
+  return cor_count, prec_length, recall_length
+
 
 def evaludate_dnn():
-  cws = DNN()
-  model = 'tmp/mlp-model9.ckpt'
-  print(cws.seg('小明来自南京师范大学',model))
+  cws = DNN('lstm', is_seg=True)
+  model = 'tmp/lstm-bmodel0.ckpt'
+  print(cws.seg('小明来自南京师范大学', model, debug=True))
   print(cws.seg('小明是上海理工大学的学生', model))
-  pre = PrepareData(4000,'pku','corpus/pku_dict.utf8',CorpusType.Test)
-  sentences = pre.raw_sentences[:100]
-  labels = pre.labels_index[:100]
+  print(cws.seg('迈向充满希望的新世纪', model))
+  print(cws.seg('我爱北京天安门', model))
+  pre = PrepareData(4000, 'pku', dict_path='corpus/pku_dict.utf8', type=CorpusType.Test)
+  sentences = pre.raw_sentences
+  labels = pre.labels_index
   corr_count = 0
   re_count = 0
   total_count = 0
 
   for _, (sentence, label) in enumerate(zip(sentences, labels)):
-    #label = np.array(list(map(lambda s: int(s), label.split(' '))))
     _, tag = cws.seg(sentence, model)
     cor_count, prec_count, recall_count = estimate_cws(tag, np.array(label))
     corr_count += cor_count
@@ -58,6 +61,7 @@ def evaludate_dnn():
   print(prec)
   print(recall)
   print(2 * prec * recall / (prec + recall))
+
 
 if __name__ == '__main__':
   evaludate_dnn()
