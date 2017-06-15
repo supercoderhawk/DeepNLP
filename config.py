543e321cae410ca7130840730c91a1e2addb1e23
supercoderhawk <supercoderhawk@gmail.com>
2017-06-15 17:22:41 +0800

    add dnn train config class

--- config.py
+++ config.py
@@ -10,3 +10,20 @@ class CorpusType(Enum):
 class TrainMode(Enum):
   Sentence = 1
   Batch = 2
+
+
+class BaseConfig:
+  def __init__(self, learning_rate, vocab_size, embed_size, hidden_units):
+    self.learning_rate = learning_rate
+    self.vocab_size = vocab_size
+    self.embed_size = embed_size
+    self.hidden_units = hidden_units
+
+
+class DNNConfig(BaseConfig):
+  def __init__(self, learning_rate, skip_left, skip_right, vocab_size, embed_size, hidden_units):
+    BaseConfig.__init__(learning_rate, vocab_size, embed_size, hidden_units)
+    self.skip_window_left = skip_left
+    self.skip_window_right = skip_right
+    self.window_size = self.skip_window_left + self.skip_window_right + 1
+    self.concat_embed_size = self.embed_size * self.window_size
