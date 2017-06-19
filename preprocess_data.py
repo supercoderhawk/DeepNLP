7fec81de71e01ebb141f44ff7fe23bcf7a692fd1
supercoderhawk <supercoderhawk@gmail.com>
2017-06-19 08:06:05 +0800

    fix bug: generate batch at skip window right equal to 0 add batch save

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,12 +1,13 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
+import os
 from config import CorpusType, TrainMode
 
 
 class PreprocessData:
   def __init__(self, corpus, mode, type=CorpusType.Train):
-    self.skip_window_left = 1
-    self.skip_window_right = 1
+    self.skip_window_left = 0
+    self.skip_window_right = 0
     self.window_size = self.skip_window_left + self.skip_window_right + 1
     self.dict_path = 'corpus/' + corpus + '_dict.utf8'
     if type == CorpusType.Train:
@@ -20,8 +21,16 @@ class PreprocessData:
     elif mode == TrainMode.Batch:
       self.characters = np.load(self.input_base + '_character_batches.npy')
       self.labels = np.load(self.input_base + '_label_batches.npy')
-      self.lengths = np.load(self.input_base+'_lengths.npy')
-      self.character_batches, self.label_batches = self.generate_batches()
+      self.lengths = np.load(self.input_base + '_lengths.npy')
+      self.output_base = 'corpus/dnn/' + corpus + '_training'
+      self.ouput_suffix = '_' + str(self.skip_window_left) + '_' + str(self.skip_window_right)
+      if os.path.exists(self.output_base + '_character_batches' + self.ouput_suffix + '.npy'):
+        self.character_batches = np.load(self.output_base + '_character_batches' + self.ouput_suffix + '.npy')
+        self.label_batches = self.output_base + '_label_batches' + self.ouput_suffix + '.npy'
+      else:
+        self.character_batches, self.label_batches = self.generate_batches()
+        np.save(self.output_base + '_character_batches' + self.ouput_suffix, self.character_batches)
+        np.save(self.output_base + '_label_batches' + self.ouput_suffix, self.label_batches)
     else:
       print('模式错误')
       exit(1)
@@ -66,11 +75,17 @@ class PreprocessData:
         extend_words = [2] * self.skip_window_left
         extend_words.extend(sentence)
         extend_words.extend([3] * self.skip_window_right)
-        word_batch = list(
-          map(lambda item: extend_words[item[0] - self.skip_window_left:item[0] + self.skip_window_right + 1],
-              enumerate(extend_words[self.skip_window_left:-self.skip_window_right], self.skip_window_left)))
+        if self.skip_window_right != 0:
+          word_batch = list(
+            map(lambda item: extend_words[item[0] - self.skip_window_left:item[0] + self.skip_window_right + 1],
+                enumerate(extend_words[self.skip_window_left:-self.skip_window_right], self.skip_window_left)))
+        else:
+          word_batch = list(
+            map(lambda item: extend_words[item[0] - self.skip_window_left:item[0] + self.skip_window_right + 1],
+                enumerate(extend_words[self.skip_window_left:], self.skip_window_left)))
         character_batch.append(word_batch)
         label_batch.append(self.labels[batch_index][sentence_index])
       character_batches.append(character_batch)
       label_batches.append(label_batch)
+
     return np.array(character_batches, dtype=np.int32), np.array(label_batches, dtype=np.int32)
