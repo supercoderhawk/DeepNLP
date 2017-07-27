093a6889660356fd0160ce810aabfca6d30635a9
supercoderhawk <supercoderhawk@gmail.com>
2017-07-27 23:09:06 +0800

    alter prepare data - remove strQ2B, add generate sentence batch

--- prepare_data.py
+++ prepare_data.py
@@ -3,13 +3,13 @@ import numpy as np
 import re
 import os
 import collections
-from utils import strQ2B
+# from utils import strQ2B
 from config import CorpusType, TrainMode
 import matplotlib.pyplot as plt
 
 
 class PrepareData:
-  def __init__(self, vocab_size, corpus, batch_length=300, batch_size=20, dict_path=None, mode=TrainMode.Batch,
+  def __init__(self, vocab_size, corpus, batch_length=100, batch_size=50, dict_path=None, mode=TrainMode.Batch,
                type=CorpusType.Train):
     self.vocab_size = vocab_size
     self.dict_path = dict_path
@@ -27,52 +27,51 @@ class PrepareData:
     elif type == CorpusType.Test:
       self.input_file = 'corpus/' + corpus + '_' + 'test.utf8'
       self.output_base = 'corpus/' + corpus + '_' + 'test_'
-    self.sentences = self.read_sentences()
+    self.lines = self.read_lines()
 
     if self.dict_path == None:
-      self.dictionary = self.build_dictionary('corpus/' + corpus + '_dict.utf8')
+      self.dictionary, self.reverse_dictionary = self.build_dictionary('corpus/' + corpus + '_dict.utf8')
     else:
-      self.dictionary = self.read_dictionary()
+      self.dictionary, self.reverse_dictionary = self.read_dictionary()
     if self.dictionary == None:
       print('vocabulary size larger than dictionary size')
       exit(1)
 
     if type == CorpusType.Train:
+      self.characters_index, self.labels_index = self.build_dataset()
       if mode == TrainMode.Sentence:
-        self.characters_index, self.labels_index = self.build_dataset()
         np.save(self.output_base + 'characters', self.characters_index)
         np.save(self.output_base + 'labels', self.labels_index)
       elif mode == TrainMode.Batch:
-        self.character_batches, self.label_batches, self.lengths = self.build_batch()
+        self.character_batches, self.label_batches, self.lengths, self.sentences, self.sentence_labels, self.sentence_lengths = self.build_batch()
         np.save(self.output_base + 'character_batches', self.character_batches)
         np.save(self.output_base + 'label_batches', self.label_batches)
         np.save(self.output_base + 'lengths', self.lengths)
     elif type == CorpusType.Test:
-      self.raw_sentences = list(map(lambda s: s.replace(self.SPLIT_CHAR, ''), self.sentences))
+      self.raw_lines = list(map(lambda s: s.replace(self.SPLIT_CHAR, ''), self.lines))
       if os.path.exists('corpus/' + corpus + '_test_labels.npy'):
         self.labels_index = np.load('corpus/' + corpus + '_test_labels.npy')
       else:
         _, self.labels_index = self.build_dataset()
         np.save('corpus/' + corpus + '_test_labels', self.labels_index)
-    '''
-    self.raw_sentences = list(map(lambda s: s.replace(self.SPLIT_CHAR, ''), self.sentences))
-    leng = list(map(lambda s:len(s),self.raw_sentences))
-    leng.sort()
-    self.plot_lengths(leng)
-    '''
 
-  def read_sentences(self):
+    # self.sentence_lengths.sort()
+    # self.plot_lengths(self.sentence_lengths)
+
+  def read_lines(self):
     file = open(self.input_file, 'r', encoding='utf-8')
     content = file.read()
-    sentences = re.sub('[ ]+', self.SPLIT_CHAR, strQ2B(content)).splitlines()  # 将词分隔符统一为双空格
-    sentences = list(filter(None, sentences))  # 去除空行
+    # sentences = re.sub('[ ]+', self.SPLIT_CHAR, strQ2B(content)).splitlines()  # 将词分隔符统一为双空格
+    sentences = re.sub('[ ]+', self.SPLIT_CHAR, content).splitlines()  # 将词分隔符统一为双空格
+    sentences = list(map(lambda s: s.strip(), filter(None, sentences)))  # 去除空行，去首尾空格
     file.close()
     return sentences
 
   def build_dictionary(self, output=None):
     dictionary = {}
-    words = ''.join(self.sentences).replace(' ', '')
+    words = ''.join(self.lines).replace(' ', '')
     vocab_count = len(collections.Counter(words))
+    print(vocab_count)
     if vocab_count + self.init_count < self.vocab_size:
       return None
     self.count.extend(collections.Counter(words).most_common(self.vocab_size - self.init_count))
@@ -83,7 +82,8 @@ class PrepareData:
       with open(output, 'w', encoding='utf8') as file:
         for ch, index in dictionary.items():
           file.write(ch + ' ' + str(index) + '\n')
-    return dictionary
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return dictionary, reverse_dictionary
 
   def read_dictionary(self):
     dict_file = open(self.dict_path, 'r', encoding='utf-8')
@@ -95,16 +95,17 @@ class PrepareData:
     dict_file.close()
     if len(dictionary) < self.vocab_size:
       return None
-    else:
-      reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-      for i in range(self.vocab_size, len(dictionary)):
-        dictionary.pop(reverse_dictionary[i])
-    return dictionary
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    # else:
+    #   reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    #   for i in range(self.vocab_size, len(dictionary)):
+    #     dictionary.pop(reverse_dictionary[i])
+    return dictionary, reverse_dictionary
 
   def build_dataset(self):
     sentence_index = []
     labels_index = []
-    for sentence in self.sentences:
+    for sentence in self.lines:
       sentence_label = []
       word_index = []
       words = sentence.strip().split(self.SPLIT_CHAR)
@@ -120,7 +121,7 @@ class PrepareData:
           sentence_label.append(3)
         for ch in word:
           index = self.dictionary.get(ch)
-          if index != None:
+          if index is not None:
             word_index.append(index)
           else:
             word_index.append(0)
@@ -129,74 +130,57 @@ class PrepareData:
     return np.array(sentence_index), np.array(labels_index)
 
   def build_batch(self):
-    character_batches = []
+    sentence_batches = []
     label_batches = []
+    sentence_lengths = []
     lengths = []
-    parts = []
+    sentences = []
+    labels = []
     unknown = 4
-    '''
-    for line in self.sentences:
-      sentences = line.split('。')
-      sections =sentences
-      #sections = list(map(lambda s: s.strip().split(','), sentences))
-      sections = list(filter(lambda s: len(s) > 0, list(map(lambda s: s.strip(), [j for i in sections for j in i]))))
-      parts.extend(sections)
-    '''
-    parts = self.sentences
-    sentence_count = len(parts)
+    seg_ch = [self.dictionary['。'], self.dictionary['！'], self.dictionary['？']]
+    no_seg_ch = [self.dictionary['”']]
+    characters_index = self.characters_index.tolist()
+    labels_index = self.labels_index.tolist()
+    line_lengths = list(map(lambda chs: len(chs), characters_index))
 
-    for part_index, part in enumerate(parts):
-      length = 0
-      real_length = 0
-      character_batch = []
-      label_batch = []
-      segments = part.split(self.SPLIT_CHAR)
-      for segment in segments:
-        segment_length = len(segment)
-        if length + segment_length > self.batch_length:
-          extra = [unknown] * (self.batch_length - length)
-          character_batch.extend(extra)
-          label_batch.extend(extra)
-          real_length = length
-          length = self.batch_length
-          break
-        elif length + segment_length <= self.batch_length:
-          length += segment_length
-          if segment_length == 0:
-            continue
-          elif segment_length == 1:
-            label_batch.append(0)
-          else:
-            label_batch.append(1)
-            label_batch.extend([2] * (segment_length - 2))
-            label_batch.append(3)
-          for ch in segment:
-            index = self.dictionary.get(ch)
-            if index != None:
-              character_batch.append(index)
-            else:
-              character_batch.append(1)
-          if length == self.batch_length:
-            real_length = length
-      if length < self.batch_length:
-        extra = [unknown] * (self.batch_length - length)
-        character_batch.extend(extra)
-        label_batch.extend(extra)
-        real_length = length
-      if real_length == 1:
-        sentence_count -= 1
+    def is_seg(item):
+      return item[1] in seg_ch and (item[0] < item[2] - 1 and characters[item[0] + 1] not in no_seg_ch)
+
+    for characters, label, length in zip(characters_index, labels_index, line_lengths):
+      if length <= 1:
         continue
-      lengths.append(real_length)
-      character_batches.append(character_batch)
-      label_batches.append(label_batch)
+      seg_indices = [0] + [i[0] + 1 for i in filter(is_seg, zip(range(length), characters, [length] * length))]
+      for pre_seg_index, cur_seg_index in zip(seg_indices[:-1], seg_indices[1:]):
+        sentence = characters[pre_seg_index:cur_seg_index]
+        sentence_labels = label[pre_seg_index:cur_seg_index]
+        sentences.append(sentence)
+        labels.append(sentence_labels)
+        sentence_length = len(sentence)
+        sentence_lengths.append(sentence_length)
+        if sentence_length <= self.batch_length:
+          pad_length = self.batch_length - sentence_length
+          sentence_batches.append(sentence + [self.dictionary['BATCH_PAD']] * pad_length)
+          label_batches.append(sentence_labels + [unknown] * pad_length)
+          lengths.append(sentence_length)
+        else:
+          if sentence_labels[sentence_length - 1] != 0 and 1 in sentence_labels[:sentence_length:-1]:
+            last_index = sentence_labels[:sentence_length:-1].index(1)
+            pad_length = self.batch_length - last_index
+            sentence_batches.append(sentence[:last_index] + [self.dictionary['BATCH_PAD']] * pad_length)
+            label_batches.append(sentence_labels[:last_index] + [unknown] * pad_length)
+            lengths.append(last_index)
+          else:
+            sentence_batches.append(sentence[:self.batch_length])
+            label_batches.append(sentence_labels[:self.batch_length])
+            lengths.append(self.batch_length)
 
-    extra_count = sentence_count % self.batch_size
-    character_batches = np.array(character_batches[:-extra_count], dtype=np.int32).reshape(
+    extra_count = len(sentence_batches) % self.batch_size
+    sentence_batches = np.array(sentence_batches[:-extra_count], dtype=np.int32).reshape(
       [-1, self.batch_size, self.batch_length])
     label_batches = np.array(label_batches[:-extra_count], dtype=np.int32).reshape(
       [-1, self.batch_size, self.batch_length])
-    lengths = np.array(lengths[:-extra_count], dtype=np.int32).reshape([-1, self.batch_size])
-    return character_batches, label_batches, lengths
+    sentence_lengths = np.array(lengths[:-extra_count], dtype=np.int32).reshape([-1, self.batch_size])
+    return sentence_batches, label_batches, sentence_lengths, sentences, labels, lengths
 
   def plot_lengths(self, lengths):
     pre_i = lengths[0]
@@ -221,5 +205,6 @@ class PrepareData:
 
 
 if __name__ == '__main__':
-  PrepareData(4000, 'pku', mode=TrainMode.Batch)
-  PrepareData(4000, 'pku', type=CorpusType.Test)
+  PrepareData(4600, 'pku', mode=TrainMode.Batch)
+  PrepareData(4000, 'pku', type=CorpusType.Test, dict_path='corpus/pku_dict.utf8')
+  PrepareData(5000, 'msr', mode=TrainMode.Batch)
