d0463670253db53a1897470b6900bac45a4d0fbd
supercoderhawk <supercoderhawk@gmail.com>
2017-07-27 23:24:07 +0800

    alter relation extraction model

--- re_cnn.py
+++ re_cnn.py
@@ -5,16 +5,17 @@ import pickle
 
 
 class RECNN():
-  def __init__(self, batch_size=50, batch_length=225):
+  def __init__(self, relation_count=2, batch_size=50, batch_length=225):
     self.dtype = tf.float32
     self.window_size = 3
     self.filter_size = 150
-    self.relation_count = 2
+    self.relation_count = relation_count
     self.batch_length = batch_length
     self.batch_size = batch_size
     self.learning_rate = 0.01
+    self.dropout_rate = 0.5
     self.lam = 0.0005
-    self.character_embed_size = 50
+    self.character_embed_size = 300
     self.position_embed_size = 50
     self.dict_path = 'corpus/emr_dict.utf8'
     self.dictionary = self.read_dictionary()
@@ -27,10 +28,10 @@ class RECNN():
     self.input_position = tf.placeholder(tf.int32, [self.batch_size, self.batch_length])
     self.input = tf.placeholder(self.dtype, [self.batch_size, self.batch_length, self.concat_embed_size, 1])
     self.input_relation = tf.placeholder(self.dtype, [self.batch_size, self.relation_count])
-    self.position_embedding = self.weight_variable([2 * self.batch_length - 1, self.position_embed_size])
+    self.position_embedding = self.weight_variable([2 * self.batch_length, self.position_embed_size])
     self.character_embedding = self.weight_variable([self.character_size, self.character_embed_size])
     self.conv_kernel = self.weight_variable([self.window_size, self.concat_embed_size, 1, self.filter_size])
-    self.bias = tf.Variable(0.1, dtype=self.dtype)
+    self.bias = self.weight_variable([self.filter_size])
     self.full_connected_weight = self.weight_variable([self.filter_size, self.relation_count])
     self.full_connected_bias = self.weight_variable([self.relation_count])
     self.position_lookup = tf.nn.embedding_lookup(self.position_embedding, self.input_position)
@@ -43,14 +44,21 @@ class RECNN():
                                                  [self.batch_size, self.batch_length, self.position_embed_size])
     self.emebd_concat = tf.expand_dims(
       tf.concat([self.character_embed_holder, self.primary_embed_holder, self.secondary_embed_holder], 2), 3)
-    self.hidden_layer = tf.squeeze(self.max_pooling(tf.nn.relu(self.conv() + self.bias)))
+    self.hidden_layer = tf.layers.dropout(tf.squeeze(self.max_pooling(tf.nn.relu(self.conv() + self.bias))),
+                                          self.dropout_rate)
+    self.output_no_softmax = tf.matmul(self.hidden_layer, self.full_connected_weight) + self.full_connected_bias
     self.output = tf.nn.softmax(tf.matmul(self.hidden_layer, self.full_connected_weight) + self.full_connected_bias)
     self.params = [self.position_embedding, self.character_embedding, self.conv_kernel, self.bias,
                    self.full_connected_weight, self.full_connected_bias]
-    self.regularization = tf.contrib.layers.apply_regularization(tf.contrib.layers.l2_regularizer(self.lam),self.params)
+    self.regularization = tf.contrib.layers.apply_regularization(tf.contrib.layers.l2_regularizer(self.lam),
+                                                                 self.params)
     self.loss = tf.reduce_sum(tf.square(self.output - self.input_relation)) / self.batch_size + self.regularization
+    self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=self.input_relation,
+                                                                 logits=self.output_no_softmax) + self.regularization
     self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
+    # self.optimizer = tf.train.AdagradOptimizer(self.learning_rate)
     self.train_model = self.optimizer.minimize(self.loss)
+    self.train_cross_entropy_model = self.optimizer.minimize(self.cross_entropy)
     self.saver = tf.train.Saver(max_to_keep=100)
 
   def weight_variable(self, shape):
@@ -68,7 +76,7 @@ class RECNN():
     with tf.Session() as sess:
       tf.global_variables_initializer().run()
       sess.graph.finalize()
-      epoches = 10
+      epoches = 100
       for i in range(epoches):
         print('epoch:' + str(i))
         for batch in self.batches:
@@ -79,8 +87,9 @@ class RECNN():
           input = sess.run(self.emebd_concat, feed_dict={self.character_embed_holder: character_embeds,
                                                          self.primary_embed_holder: primary_embeds,
                                                          self.secondary_embed_holder: secondary_embeds})
-          sess.run(self.train_model, feed_dict={self.input: input, self.input_relation: batch['label']})
-        self.saver.save(sess, self.output_folder + 'cnn_model%d.ckpt' % i)
+          # sess.run(self.train_model, feed_dict={self.input: input, self.input_relation: batch['label']})
+          sess.run(self.train_cross_entropy_model, feed_dict={self.input: input, self.input_relation: batch['label']})
+        self.saver.save(sess, self.output_folder + 'cnn_emr_model%d.ckpt' % i)
 
   def load_batches(self):
     with open(self.batch_path, 'rb') as f:
@@ -99,11 +108,11 @@ class RECNN():
 
   def test(self):
     with tf.Session() as sess:
-      self.saver.restore(sess, self.output_folder + 'cnn_model9.ckpt')
+      self.saver.restore(sess, self.output_folder + 'cnn_emr_model5.ckpt')
       neg = 0
       all = 0
-      # print(self.batches[0])
-      for i in range(500):
+
+      for i in range(10):
         batch = self.batches[i]
         character_embeds, primary_embeds = sess.run([self.character_lookup, self.position_lookup],
                                                     feed_dict={self.input_characters: batch['sentence'],
@@ -116,12 +125,15 @@ class RECNN():
         neg += np.count_nonzero(np.argmax(output, 1) - np.argmax(batch['label'], 1))
         if np.sum(np.argmax(output, 1)) > 0:
           print('a')
+
+        print(np.argmax(output, 1))
+        # print(np.argmax(batch['label'], 1))
         all += self.batch_size
+
       prec = 1 - neg / all
       print(prec)
 
 
 if __name__ == '__main__':
   reCNN = RECNN()
-  # reCNN.train()
-  reCNN.test()
+  reCNN.train()
